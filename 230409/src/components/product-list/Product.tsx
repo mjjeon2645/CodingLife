ae144a21893f9e28b9feadde90b33d99d47a1f87
Jeon Minji <mjjeon2645@gmail.com>
2023-04-09 21:39:09 +0900

    230409-프론트엔드 생존코스 9주차 강의 따라해보기

---
+++ 230409/src/components/product-list/Product.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import { ProductSummary } from '../../types';
+
+import numberFormat from '../../utils/numberFormat';
+
+const Thumbnail = styled.img.attrs({
+  alt: 'Thumbnail',
+})`
+    display: block;
+    width: 100%;
+    aspect-ratio: 1/1;
+  `;
+
+  type ProductProps = {
+    product: ProductSummary;
+  }
+
+export default function Product({ product }: ProductProps) {
+  return (
+    <div>
+      <Thumbnail src={product.thumbnail.url} />
+      <div>{product.name}</div>
+      <div>
+        {numberFormat(product.price)}
+        원
+      </div>
+    </div>
+  );
+}
