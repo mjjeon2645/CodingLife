69f6f4a2f09e02e4bdb8184143813ed77f02f178
Jeon Minji <mjjeon2645@gmail.com>
2023-05-11 10:42:02 +0900

    React Router 설치 후 기본 세팅(routes, layout, outlet)

--- 230511/src/main.tsx
+++ 230511/src/main.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import routes from './routes';
+
+const router = createBrowserRouter(routes);
 
 export default function main() {
   const element = document.getElementById('root');
@@ -11,7 +16,11 @@ export default function main() {
 
   const root = ReactDOM.createRoot(element);
 
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
 }
 
 main();
