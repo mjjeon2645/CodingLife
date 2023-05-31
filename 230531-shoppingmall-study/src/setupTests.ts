1e22d6d594facf288d7c233a158a427c5ef4df86
Jeon Minji <mjjeon2645@gmail.com>
2023-05-31 12:34:30 +0900

    jest-dom을 setupTests 파일에 적용(RTL에서 활용할 수 있는 DOM 확인용 Matchers)

--- 230531-shoppingmall-study/src/setupTests.ts
+++ 230531-shoppingmall-study/src/setupTests.ts
@@ -1,4 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import 'reflect-metadata';
+
+import '@testing-library/jest-dom';
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
